640011831d4fc668b64a36c6e3b4d176225eb246
Declan Scott <Datoneguy246@gmail.com>
2023-01-30 08:17:02 -0500

    initial

---
+++ src/Codes.java
@@ -0,0 +1,12 @@
+// Binary Tree for all codes
+
+import com.sun.source.tree.BinaryTree;
+
+public class Codes {
+    private BinaryTree codeValues;
+
+    public String getCode(String routeName)
+    {
+
+    }
+}
