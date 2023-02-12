eff433384d209a94062f0be7f1c10dbb35b33898
stefanoking28 <48166174+stefanoking28@users.noreply.github.com>
2023-02-12 13:42:35 -0500

    main added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        Boolean run = true;
+        String trainCompanyName;
+        String companyCode;
+        String check = null;
+        Boolean prompt;
+        while(run) {
+            while(!(check.equals("train company")||(check.equals("search")))){
+                check = Input.getString("Create a new train company or search (train company or search)");
+            }
+            if(check.equals("train company")){
+                trainCompanyName = Input.getString("What is the name of your train company");
+                companyCode = Input.getString("what is the code of the company");
+            }else{
+                prompt = true;
+                while (prompt) {
 
+                }
+            }
+        }
     }
 }
