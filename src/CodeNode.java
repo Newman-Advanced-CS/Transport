3b8a881704c742a2fe79147fd3b8d28f86927e9c
Declan Scott <Datoneguy246@gmail.com>
2023-02-13 09:39:40 -0500

    Binary tree

--- src/CodeNode.java
+++ src/CodeNode.java
@@ -2,26 +2,50 @@ import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
-public record CodeNode(String routeName, String routeCode) {
-    @Override
-    public String routeName() {
+public class CodeNode {
+    private final String routeName;
+    private final String routeCode;
+
+    private CodeNode right = null;
+    private CodeNode left = null;
+
+    public String getRouteName() {
         return routeName;
     }
-
-    @Override
-    public String routeCode() {
+    public String getRouteCode() {
         return routeCode;
+    }
+
+    public CodeNode getRight() {
+        return right;
+    }
+
+    public void setRight(CodeNode right) {
+        this.right = right;
+    }
+
+    public CodeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(CodeNode left) {
+        this.left = left;
+    }
+
+    public CodeNode(String routeName, String routeCode) {
+        this.routeName = routeName;
+        this.routeCode = routeCode;
     }
 
     // Returns whichever node is larger (to the right)
     public CodeNode compare(CodeNode other) throws UnsupportedEncodingException {
         int thisKey = numberfy(routeName);
-        int otherKey = numberfy(other.routeName());
+        int otherKey = numberfy(other.getRouteName());
 
         return thisKey > otherKey ? this : other;
     }
 
-    public int numberfy(String input)
+    public static int numberfy(String input)
     {
         String bytes = Arrays.toString(input.getBytes(StandardCharsets.US_ASCII));
         bytes = bytes.substring(1, bytes.length()-1);
--- src/CodesBinaryTree.java
+++ src/CodesBinaryTree.java
@@ -1,17 +1,60 @@
 // Binary Tree for all codes
 
-import com.sun.source.tree.BinaryTree;
+import java.io.UnsupportedEncodingException;
 
 public class CodesBinaryTree {
-    private BinaryTree codeValues;
+    private CodeNode codeValuesRoot;
+
+    public CodesBinaryTree(CodeNode codeValuesRoot) {
+        this.codeValuesRoot = codeValuesRoot;
+    }
+
+    public CodesBinaryTree() {}
 
     public String getCode(String routeName)
     {
-        return "";
+        CodeNode searchPosition = codeValuesRoot;
+        while (!searchPosition.getRouteName().equals(routeName))
+        {
+            if (CodeNode.numberfy(routeName) > CodeNode.numberfy(searchPosition.getRouteName())){
+                // It is to the right
+                searchPosition = searchPosition.getRight();
+            }else{
+                // It is to the left
+                searchPosition = searchPosition.getLeft();
+            }
+        }
+        return searchPosition.getRouteCode();
     }
 
-    public void addCode(CodeNode node)
-    {
-    //    codeValues
+    public void addCode(CodeNode node) throws UnsupportedEncodingException {
+        // First case is when codeValuesRoot doesn't exist
+        if (codeValuesRoot == null)
+        {
+            codeValuesRoot = node;
+            return;
+        }
+
+        CodeNode nextNode = codeValuesRoot;
+        boolean foundPosition = false;
+        while (!foundPosition)
+        {
+            boolean isThisLarger = nextNode.compare(node) == node; // if the inputted node is larger than the next node
+            CodeNode targetNode = isThisLarger ? nextNode.getRight() : nextNode.getLeft();
+            if (targetNode == null)
+            {
+                // Insert at that position
+                foundPosition = true;
+                if (isThisLarger)
+                {
+                    nextNode.setRight(node);
+                }else{
+                    nextNode.setLeft(node);
+                }
+            }else{
+                // Go to that node
+                nextNode = targetNode;
+            }
+        }
     }
 }
